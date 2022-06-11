988694e725450da3f93faf8ee8be167d6c360085
Delyan <ddelchev>
2022-06-11 16:26:39 +0200

    final 1

--- src/addBookingPanel.java
+++ src/addBookingPanel.java
@@ -2,6 +2,20 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.*;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class addBookingPanel {
     private JPanel panel1;
@@ -11,6 +25,7 @@ public class addBookingPanel {
     private JLabel addBookingPanel1;
     private JTextField textField4;
     private JButton acceptButton;
+    private JTextArea textArea1;
 
     addBookingPanel() {
         JFrame frame = new JFrame("Add booking Panel");
@@ -30,10 +45,21 @@ public class addBookingPanel {
                     mIDnhIDohID[2] = textField3.getText();
                     mIDnhIDohID[3]= textField4.getText();
                     Connection c = DatabaseController.getInstance().getConnectionOrNull("jdbc:oracle:thin:@StudiDB.GM.TH-Koeln.de:1521:vlesung");
-                    DatabaseController.Insert(c,"BUCHUNG",Integer.parseInt(mIDnhIDohID[0]),Integer.parseInt(mIDnhIDohID[1]),mIDnhIDohID[2],mIDnhIDohID[3]);
+                    DatabaseController.Insert(c,"BUCHUNG",Integer.parseInt(mIDnhIDohID[0]),Integer.parseInt(mIDnhIDohID[1]),null,null);
+                    try {
+                        mIDnhIDohID[1] = textField2.getText();
+                        Statement stmt=c.createStatement();
+                        ResultSet rs=stmt.executeQuery("select * from KUNDE WHERE NUTZER_ID = "+ mIDnhIDohID[1]);
+                        while(rs.next())
+                        textArea1.setText("Kunde " + rs.getInt("NUTZER_ID")+" hat nun "+ rs.getInt(7)+" treuepunkte");
+                    }
+                    catch (SQLException throwables) {
+                        throwables.printStackTrace();
+                    }
 
 
-            }
+
+                }
             }
         });
     }
